41d7e2d95c481ff37f5759fa35b0511756d5c531
akirov <asensi72@yahoo.com>
2024-04-01 21:46:58 +0300

    Extended tesserocr example

--- python/img_utils.py
+++ python/img_utils.py
@@ -1,4 +1,18 @@
 import cv2
+from PIL import Image
+import numpy as np
+
+
+def read_and_convert_PIL(image_uri):
+    image_arr = np.asarray(Image.open(image_uri).convert('RGB'), dtype=np.uint8)
+    return Image.fromarray(image_arr)
+
+
+def read_and_convert_OCV_to_PIL(image_uri):
+    img = cv2.imread(image_uri, cv2.IMREAD_COLOR)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # OpenCV default format is BGR
+    return Image.fromarray(img_rgb)
+
 
 def detect_plates_ocv(image, cascade_uri):
     carplate_haar_cascade = cv2.CascadeClassifier(cascade_uri)
@@ -10,3 +24,5 @@ def detect_plates_ocv(image, cascade_uri):
     for x,y,w,h in plate_rects:  # 2D numpy array, each row is [x y w h]
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
     return plate_rects, img
+
+
--- python/try_tessocr.py
+++ python/try_tessocr.py
@@ -2,30 +2,48 @@ import os
 import sys
 from PIL import Image
 import matplotlib.pyplot as plt
-import cv2
+from img_utils import read_and_convert_PIL, read_and_convert_OCV_to_PIL
 
 if 'TESSDATA_PREFIX' not in os.environ:
     os.environ['TESSDATA_PREFIX'] = r'../tesseract/tessdata'
 
 import tesserocr as tocr
 
-
 print(tocr.get_languages())
 
-images = ['../data/test/bul_001.jpg', '../data/test/bul_001_seg.jpg', '../data/test/bul_001_seg.png']
 
-with tocr.PyTessBaseAPI() as api:
-    for img in images:
-        api.SetImageFile(img)
-        print("Image: ", img)
-        print("Text: '", api.GetUTF8Text(), "'")
-        print("Confidence per word: ", api.AllWordConfidences())
-# api is automatically finalized when used in a with-statement (context manager).
-# otherwise api.End() should be explicitly called when it's no longer needed.
+images = ['../data/test/bul_001.jpg', '../data/test/bul_001_seg.jpg']  # , '../data/test/bul_001_seg.png'
 
-for img in images:
-    print(img, tocr.file_to_text(img))
-    image = Image.open(img)
+for img_uri in images:
+    print("\nfile_to_text(", img_uri, ") = '", tocr.file_to_text(img_uri), "'")
+    image = Image.open(img_uri)
     plt.imshow(image)
     plt.show()
-    print(img, tocr.image_to_text(image))
+    print("PIL image_to_text(", img_uri, ") = '", tocr.image_to_text(image), "'")
+
+print("\n\nUsing PyTessBaseAPI")
+with tocr.PyTessBaseAPI(lang='eng', psm=7, oem=3) as api:
+    api.SetVariable("tessedit_char_whitelist", "ABCEHKMOPTXY0123456789 .")
+    for img_uri in images:
+        api.SetImageFile(img_uri)
+        print("\nSetImageFile: ", img_uri)
+        print("GetUTF8Text: '", api.GetUTF8Text(), "'")
+        print("Confidence per word: ", api.AllWordConfidences())
+
+        api.SetImage(Image.open(img_uri))
+        print("SetImage(PIL image): ", img_uri)
+        print("GetUTF8Text: '", api.GetUTF8Text(), "'")  # Not recognized without psm=7
+        print("Confidence per word: ", api.AllWordConfidences())
+
+        api.SetImage(read_and_convert_PIL(img_uri))
+        print("SetImage(read_and_convert_PIL): ", img_uri)
+        print("GetUTF8Text: '", api.GetUTF8Text(), "'")
+        print("Confidence per word: ", api.AllWordConfidences())
+
+        api.SetImage(read_and_convert_OCV_to_PIL(img_uri))
+        print("SetImage(read_and_convert_OCV_to_PIL): ", img_uri)
+        print("GetUTF8Text: '", api.GetUTF8Text(), "'")
+        print("Confidence per word: ", api.AllWordConfidences())
+
+# api is automatically finalized when used in a with-statement (context manager).
+# otherwise api.End() should be explicitly called when it's no longer needed.
